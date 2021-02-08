20f897eb0153d2b7c7d6e741be2e2bafec57c981
AdamWeissman <adam@comedytragedyepic.com>
2021-02-08 00:29:19 -0500

    additional mutations for the sample

--- fatTony.js
+++ fatTony.js
@@ -1,5 +1,7 @@
 const fetch = require('node-fetch');
 
+"use strict"
+
 function makeTheDrop (url) {
     return fetch(url)
       .then(data => data.json())
@@ -11,13 +13,13 @@ function theCouriersDeliver(dahShipment) {
   dahShipment.forEach((shadyPackage) => { 
     warehouse.push(makeTheDrop(...Object.values(shadyPackage)))
   });
-  return Promise.all(warehouse)
+  return Promise.all(warehouse) // FAT TONY SAYS: "DONT MAKE NO PROMISES YOU CAN'T KEEP"
 }
 
-function tonysApprenticeCatalogs(theShat, thePackages){
+function tonysApprenticeCatalogs(allShadyPackages, dahShipment){
   const theBookiesLedger = {}
-  for (let i = 0; i < theShat.length; i++){
-    theBookiesLedger[Object.keys(thePackages[i])] = theShat[i]
+  for (let i = 0; i < allShadyPackages.length; i++){
+    theBookiesLedger[Object.keys(dahShipment[i])] = allShadyPackages[i]
   }
   return theBookiesLedger
 }
@@ -35,10 +37,18 @@ function fatTony(dahShipment, theMainAttraction) {
 function theMainAttraction(theLedger){ // THIS IS YOUR MAIN LOOP
   //YOU CAN NOW MUTATE DATA AS IF IT WERE A REGULAR VARIABLE!!!
   const localLedger = theLedger
-  console.log(localLedger)
+  console.log("\n\n")
+  console.log("FIRST RUN", localLedger)
+  console.log("\n\n")
   localLedger["sampleBot"].theeResponseObject[0] = "I CHANGED TO THIS"  
-  console.log(localLedger)
-  console.log("WRITE THE REST OF YOUR PROGRAM IN HERE")
+  localLedger["sampleBot"].theeResponseObject[1] = "I CHANGED THIS, TOO"
+  console.log("THIS IS THE ARRAY", localLedger["sampleBot"].theeResponseObject[6])
+  localLedger["sampleBot"].theeResponseObject[6] = localLedger["sampleBot"].theeResponseObject[6].map((x) => {return x.toUpperCase()})
+  console.log("THIS IS THE ARRAY CHANGED", localLedger["sampleBot"].theeResponseObject[6])
+  console.log("\n\n")
+  console.log("SECOND RUN AFTER ALTERED DATA", localLedger)
+  console.log("\n\n")
+  // WRITE THE REST OF YOUR PROGRAM INSIDE THE MAIN ATTRACTION, OR WHATEVER YOU WANT TO CALL IT
 }
 
 const theShipment = [
